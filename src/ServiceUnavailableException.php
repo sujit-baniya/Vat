78656b382d892d82f6679366d4a52c9e375d0382
TPWeb <info@tpweb.org>
2019-12-18 20:50:20 +0100

    Create ServiceUnavailableException.php

---
+++ src/ServiceUnavailableException.php
@@ -0,0 +1,13 @@
+<?php
+namespace TPWeb\Vat;
+
+
+use Exception;
+
+class ServiceUnavailableException extends Exception
+{
+    public function __construct($message, Exception $previous = null)
+    {
+        parent::__construct($message, 0, $previous);
+    }
+}
